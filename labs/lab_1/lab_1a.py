ab81066ac0b2a1d46f42d14c2a433e74423eb583
yerikim037 <yerikim037@gmail.com>
2026-02-06 23:31:07 -0500

    added personal introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Yeri" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hello everyone! My name is Yeri Kim. I am South Korean, but I currently attend a boarding school in New York. I’m interested in learning about robotics and rocketry. I’m part of my school’s rocketry team and FTC robotics team, which have introduced me to many fascinating topics. I hope to meet you all soon!")
 if __name__ == "__main__":
     main()
